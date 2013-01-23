e9d87ca2955ecbd65003690985dbc0f3c4c2f9dc
Jan Erik Solem <jesolem@gmail.com>
2013-01-23 14:24:32 +0100

    Missing square of trace in Harris.

--- PCV/localdescriptors/harris.py
+++ PCV/localdescriptors/harris.py
@@ -22,7 +22,7 @@ def compute_harris_response(im,sigma=3):
     Wdet = Wxx*Wyy - Wxy**2
     Wtr = Wxx + Wyy
     
-    return Wdet / Wtr
+    return Wdet / (Wtr*Wtr)
    
     
 def get_harris_points(harrisim,min_dist=10,threshold=0.1):

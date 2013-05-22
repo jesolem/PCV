7635885369d382c42423a8863d6fab18740f7ac8
Jan Erik Solem <jesolem@gmail.com>
2013-05-22 17:53:17 +0200

    Updated matching graph example.

--- examples/ch2_matching_graph.py
+++ examples/ch2_matching_graph.py
@@ -12,12 +12,17 @@ This is the example graph illustration of matching images from Figure 2-10.
 To download the images, see ch2_download_panoramio.py.
 """
 
-download_path = "" # set this to the path where you downloaded the panoramio images
-path = "/tmp/panoramio/" # path to save thumbnails
+download_path = "panoimages" # set this to the path where you downloaded the panoramio images
+path = "/FULLPATH/panoimages/" # path to save thumbnails (pydot needs the full system path)
 
 # list of downloaded filenames
 imlist = imtools.get_imlist(download_path)
 nbr_images = len(imlist)
+
+# extract features
+featlist = [imname[:-3]+'sift' for imname in imlist]
+for i,imname in enumerate(imlist):
+    sift.process_image(imname, featlist[i])
 
 matchscores = zeros((nbr_images,nbr_images))
 
@@ -36,7 +41,6 @@ for i in range(nbr_images):
     for j in range(i+1,nbr_images): # no need to copy diagonal
         matchscores[j,i] = matchscores[i,j]
 
-
 threshold = 2 # min number of matches needed to create link
 
 g = pydot.Dot(graph_type='graph') # don't want the default directed graph 
@@ -47,16 +51,16 @@ for i in range(nbr_images):
             # first image in pair
             im = Image.open(imlist[i])
             im.thumbnail((100,100))
-            filename = str(i)+'.png'
+            filename = path+str(i)+'.png'
             im.save(filename) # need temporary files of the right size 
-            g.add_node(pydot.Node(str(i),fontcolor='transparent',shape='rectangle',image=path+filename))
+            g.add_node(pydot.Node(str(i),fontcolor='transparent',shape='rectangle',image=filename))
     
             # second image in pair
             im = Image.open(imlist[j])
             im.thumbnail((100,100))
-            filename = str(j)+'.png'
+            filename = path+str(j)+'.png'
             im.save(filename) # need temporary files of the right size 
-            g.add_node(pydot.Node(str(j),fontcolor='transparent',shape='rectangle',image=path+filename)) 
+            g.add_node(pydot.Node(str(j),fontcolor='transparent',shape='rectangle',image=filename)) 
             
             g.add_edge(pydot.Edge(str(i),str(j)))
 

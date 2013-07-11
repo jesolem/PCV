f8330c183cefea8b4418ace34b043ebc640b0428
Jan Erik Solem <jesolem@gmail.com>
2013-07-11 13:02:57 +0200

    Update morphology thresholding.

--- examples/ch1_morphology.py
+++ examples/ch1_morphology.py
@@ -9,7 +9,7 @@ This is the morphology counting objects example in Section 1.4.
 
 # load image and threshold to make sure it is binary
 im = array(Image.open('../data/houses.png').convert('L'))
-im = 1*(im<128)
+im = (im<128)
 
 labels, nbr_objects = measurements.label(im)
 print "Number of objects:", nbr_objects
